253d97cb446832fb3c27c9de16e61462be74b8f2
1371744007-hash <1371744007@qq.com>
2026-09-12 01:44:13 +0800

    Prepare Render deployment (launcher.js)

--- launcher.js
+++ launcher.js
@@ -9,6 +9,10 @@ if(fs.existsSync(envFile)){
     const m=line.match(/^([A-Z0-9_]+)=(.*)$/);if(m&&!env[m[1]])env[m[1]]=m[2].trim();
   }
 }
-const child=spawn(process.execPath,['--use-env-proxy',path.join(__dirname,'server.js')],{stdio:'inherit',env});
+const args=[];
+const wantsProxy=Boolean(env.HTTPS_PROXY||env.HTTP_PROXY);
+if(wantsProxy&&process.allowedNodeEnvironmentFlags?.has('--use-env-proxy'))args.push('--use-env-proxy');
+args.push(path.join(__dirname,'server.js'));
+const child=spawn(process.execPath,args,{stdio:'inherit',env});
 child.on('exit',code=>process.exit(code??0));
 for(const signal of ['SIGINT','SIGTERM'])process.on(signal,()=>child.kill(signal));
